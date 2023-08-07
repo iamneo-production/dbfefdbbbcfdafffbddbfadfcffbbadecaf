001b0e4b550f466e2810d43a74905409ccca537e
Iamneo <techadmin@iamneo.ai>
2023-08-07 10:04:29 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Cricketer.java
+++ springapp/src/main/java/com/examly/springapp/Cricketer.java
@@ -1,7 +1,13 @@
-
+package com.examly.springapp;
 
 public class Cricketer {
     String name;
     int age;
     String country;
+
+    public Cricketer(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
 }
--- springapp/src/test/java/com/examly/springapp/CricketerTest.java
+++ springapp/src/test/java/com/examly/springapp/CricketerTest.java
@@ -3,6 +3,7 @@ package com.examly.springapp;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Collections;
+import com.examly.springapp.Cricketer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
